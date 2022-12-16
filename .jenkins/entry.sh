e0b65c1ee28fafa50ad630bf028a6d58d9e1cdc2
Pansysk75 <pansysk75@gmail.com>
2022-12-16 14:35:15 +0200

    Fix directories

--- .jenkins/entry.sh
+++ .jenkins/entry.sh
@@ -3,4 +3,4 @@
 # Activate python virtual env
 source /home/panos/Desktop/virtual_env/bin/activate
 pwd
-source launch_python.sh
+source .jenkins/launch_python.sh
--- .jenkins/launch_python.sh
+++ .jenkins/launch_python.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./script.py
+./.jenkins/script.py
